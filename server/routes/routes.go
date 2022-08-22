9f6e76e22401fa89bd479d2812fbce5a6a00bef3
Gabriel Rabelo <contato.ideialisa@gmail.com>
2022-08-22 16:19:56 -0300

    chore: add cors

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -9,7 +9,25 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func ServerRoutes(router *gin.Engine, hub *socket.Hub) *gin.Engine {
+	router.Use(CORSMiddleware())
+
 	// healthcheck
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
