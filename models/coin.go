5e20dc051805d2d2709d2195cdc2f9134162eabf
Gabriel Rabelo <contato.ideialisa@gmail.com>
2022-08-21 23:31:10 -0300

    fix: set unique key from coin code

--- models/coin.go
+++ models/coin.go
@@ -9,7 +9,7 @@ import (
 type Coin struct {
 	Id        uint           `json:"id" gorm:"primaryKey unique autoIncrementIncrement"`
 	Name      string         `json:"name"`
-	Code      string         `json:"code"`
+	Code      string         `json:"code" gorm:"unique"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
